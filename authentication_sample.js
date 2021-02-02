e3da92f053ec5e04cbe690035206e2b451f285f7
Josue(MSFT) <v-zhli15@microsoft.com>
2021-02-02 09:25:41 +0800

    Update authentication_sample.js

--- authentication_sample.js
+++ authentication_sample.js
@@ -2,8 +2,6 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See LICENSE in the project root for
  * license information.
- * 
- * This file uses an outdated library.  Please see the readme to find the latest version.
  */
 'use strict';
 
